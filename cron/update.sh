33e5bf76f9c889eecd41bf8ce9b3df109ff73c37
tyage <namatyage@gmail.com>
2015-01-30 15:13:45 +0900

    update VPN cron file

--- cron/update.sh
+++ cron/update.sh
@@ -3,4 +3,4 @@
 cd `dirname $0`
 ipAddress=$(curl ifconfig.me 2>/dev/null)
 clientFile='home.tblk/client.conf'
-sed -i -e "s/^remote \([^ ]\+\) \([^ ]\+\)$/remote $ipAddress \2/" $clientFile 
+sed -i -e "s/^remote \([^ ]*\) \([^ ]\+\)$/remote $ipAddress \2/" $clientFile 
